6318e999a58ae91d014dfd985299802def63ac0f
Mohamad Ardi <mohamadardi.ilkom12@gmail.com>
2020-10-20 01:27:30 +0800

    Update interpolasi-string.dart

--- interpolasi-string.dart
+++ interpolasi-string.dart
@@ -1,3 +1,6 @@
+// Hacktoberfest2020
+// Support open source and pick a limited edition T-shirt or plant a tree.
+
 void main() {
   var a = 2;
   var b = 3;
@@ -6,6 +9,6 @@ void main() {
   var d = 'Dart';
 
   // memanggil metode di dalam string
-  print('Interpolasi string dalam ${d.toUpperCase()}');
+  print('Interpolasi string for a limited edition T-shirt ${d.toUpperCase()}');
   print(c);
 }
